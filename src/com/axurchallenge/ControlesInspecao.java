8040250e89bd1269f886e8f679adece7f47885c9
WilliRamon <acdc.ramon@hotmail.com>
2022-02-16 22:40:03 -0300

    Implementando Interface e classe Inspeção

---
+++ src/com/axurchallenge/ControlesInspecao.java
@@ -0,0 +1,9 @@
+package com.axurchallenge;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public interface ControlesInspecao {
+
+	public abstract void analise() throws MalformedURLException, IOException;
+}
--- src/com/axurchallenge/Inspecao.java
+++ src/com/axurchallenge/Inspecao.java
@@ -6,34 +6,49 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
-class Inspecao implements InterFace{
+class Inspecao implements ControlesInspecao {
 
 	@Override
-	public void analise(URL url) throws MalformedURLException, IOException {
+	public void analise() throws MalformedURLException, IOException {
 		boolean suspicious = false;
-		
+		boolean urlInvalida = false;
+		String urlUsuario;
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Por favor, insira uma url para inspeção: ");
+		do {
+			urlUsuario = ler.nextLine();
+			urlInvalida = false;
+
+			try {
+				URL url = new URL(urlUsuario);
+			} catch (MalformedURLException e) {
+				System.out.println("URL invalida. Digite novamente.");
+				urlInvalida = true;
+			}
+		} while (urlInvalida == true);
+
+		URL url = new URL(urlUsuario);
 		InputStream is = url.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		String linha = br.readLine();
-		
-		while(linha!=null){
-			
+
+		while (linha != null) {
+
 			if ((linha.contains("senha")) || (linha.contains("black friday")) || (linha.contains("promoção"))) {
 				suspicious = true;
 			}
 			linha = br.readLine();
 		}
-		
-		if(suspicious==true)
-		{
-			System.out.println("suspicious");
-		}else
-		{
-			System.out.println("safe");
+
+		if (suspicious == true) {
+			System.out.println("Suspicious - Detectada expressão/palavra suspeita");
+		} else {
+			System.out.println("Safe - Não possui ecpressões suspeitas");
 		}
 	}
-
 
 }
